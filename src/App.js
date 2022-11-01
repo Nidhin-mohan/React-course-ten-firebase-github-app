9d1e299ef50c2e99b6927a19430842d37bdd2371
Nidhin-mohan <nidhinmohannidhin>
2022-11-01 10:54:04 +0530

    Fixing Router issue

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter as Router , Routes, Route, Link} from "react-router-dom"
+import { Routes, Route, Link} from "react-router-dom"
 import {ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.min.css"
 
@@ -24,18 +24,18 @@ const [user, setUser] = useContext(null);
 
 return (
   <div className="App">
-   
-    <Router>
-      <ToastContainer />
-      <UserContext.Provider value={{ user, setUser }}>
+    <ToastContainer />
+
+    
+      {/* <UserContext.Provider value={{ user, setUser }}> */}
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/signin" element={<Signin />} />
-          <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="*" element={<PageNotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
-      </UserContext.Provider>
-    </Router>
+      {/* </UserContext.Provider> */}
+  
   </div>
 );
 
